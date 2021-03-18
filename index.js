190c707a77bf01efe709658c42e9e63320c500f8
Dzenan Helic <dzenan.helic@gmail.com>
2021-03-18 08:27:28 +0100

    Added changes to index.js

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 const AmiClient = require('asterisk-ami-client');
-let client = new AmiClient();
+const WebSocket = require('ws');
 
-client.connect('user', 'secret', {
+const wss = new WebSocket.Server({ port: 8080 });
+
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incomin(message) {
+        console.log('received: %s', message);
+    });
+
+    let client = new AmiClient();
+
+    client.connect('admin', '123456', {
         host: 'localhost',
         port: 5038
     })
-    .then(amiConnection => {
-
-        client
-            .on('connect', () => console.log('connect'))
-            .on('event', event => console.log(event))
-            .on('data', chunk => console.log(chunk))
-            .on('response', response => console.log(response))
-            .on('disconnect', () => console.log('disconnect'))
-            .on('reconnection', () => console.log('reconnection'))
-            .on('internalError', error => console.log(error))
-            .action({
-                Action: 'Ping'
-            });
-
-        setTimeout(() => {
-            client.disconnect();
-        }, 5000);
-
-    })
-    .catch(error => console.log(error));
+        .then(amiConnection => {
+            client
+                .on('connect', () => console.log('connect'))
+                .on('event', event => {
+                    console.log('event');
+                    console.log(event);
+                    ws.send(JSON.stringify(event));
+                })
+                .on('response', response => {
+                    console.log(response)
+                    console.log('response')
+                    ws.send(JSON.stringify(response));
+                })
+                .on('disconnect', () => {
+                    ws.send("0");
+                    console.log('disconnect')
+                })
+                .on('reconnection', () => {
+                    ws.send("1");
+                    console.log('reconnection')
+                })
+                .on('internalError', error => console.log(error))
+                .action({
+                    Action: 'Ping'
+                });
+        })
+        .catch(error => console.log(error));
+});
